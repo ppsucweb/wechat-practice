4aed67e374a695b71bfda2212f80154a5fc7b039
LWD666 <39625238+LWD666@users.noreply.github.com>
2019-02-15 23:57:47 +0800

    2.15 updata 2

--- html/moments/app.js
+++ html/moments/app.js
@@ -2,7 +2,6 @@
 var userName = 'Lu仔酱';
 // 朋友圈页面的数据
 var data = [{
-<<<<<<< HEAD
 	user: {
 		name: '阳和',
 		avatar: './img/avatar2.png'
@@ -88,86 +87,6 @@ var data = [{
 		}]
 	}
 }];
-=======
-  user: {
-    name: '阳和', 
-    avatar: './img/avatar2.png'
-  }, 
-  content: {
-    type: 0, // 多图片消息
-    text: '华仔真棒，新的一年继续努力！',
-    pics: ['./img/reward1.png', './img/reward2.png', './img/reward3.png', './img/reward4.png'],
-    share: {},
-    timeString: '3分钟前'
-  }, 
-  reply: {
-    hasLiked: false,
-    likes: ['Guo封面', '源小神'],
-    comments: [{
-      author: 'Guo封面',
-      text: '你也喜欢华仔哈！！！'
-    },{
-      author: '喵仔zsy',
-      text: '华仔实至名归哈'
-    }]
-  }
-}, {
-  user: {
-    name: '伟科大人',
-    avatar: './img/avatar3.png'
-  },
-  content: {
-    type: 1, // 分享消息
-    text: '全面读书日',
-    pics: [],
-    share: {
-      pic: 'http://coding.imweb.io/img/p3/transition-hover.jpg',
-      text: '飘洋过海来看你'
-    },
-    timeString: '50分钟前'
-  },
-  reply: {
-    hasLiked: false,
-    likes: ['阳和'],
-    comments: []
-  }
-}, {
-  user: {
-    name: '深圳周润发',
-    avatar: './img/avatar4.png'
-  },
-  content: {
-    type: 2, // 单图片消息
-    text: '很好的色彩',
-    pics: ['http://coding.imweb.io/img/default/k-2.jpg'],
-    share: {},
-    timeString: '一小时前'
-  },
-  reply: {
-    hasLiked: false,
-    likes:[],
-    comments: []
-  }
-}, {
-  user: {
-    name: '喵仔zsy',
-    avatar: './img/avatar5.png'
-  },
-  content: {
-    type: 3, // 无图片消息
-    text: '以后咖啡豆不敢浪费了',
-    pics: [],
-    share: {},
-    timeString: '2个小时前'
-  }, 
-  reply: {
-    hasLiked: false,
-    likes:[],
-    comments: []
-  }
-}];
-
->>>>>>> b15c21a11036c9e4ed7e12bc6ebe3b9c31c48318
 // 相关 DOM
 var $page = document.querySelector('.page-moments');
 var $momentsList = document.querySelector('.moments-list');
@@ -177,7 +96,6 @@ var $momentsList = document.querySelector('.moments-list');
  * @param {Array} likes 点赞人列表
  * @return {String} 返回html字符串
  */
-<<<<<<< HEAD
 function likesHtmlTpl(likes, comments) {
 	/*if (!likes.length) {
 		return '';
@@ -195,24 +113,6 @@ function likesHtmlTpl(likes, comments) {
 	htmlText.push('<a id="new-like" value=1 class="reply-who" href="#"></a>');
 	htmlText.push('</div>');
 	return htmlText.join('');
-=======
-function likesHtmlTpl(likes) {
-  if (!likes.length) {
-    return '';
-  }
-  var  htmlText = ['<div class="reply-like"><i class="icon-like-blue"></i>'];
-  // 点赞人的html列表
-  var likesHtmlArr = [];
-  // 遍历生成
-  for(var i = 0, len = likes.length; i < len; i++) {
-    likesHtmlArr.push('<a class="reply-who" href="#">' + likes[i] + '</a>');
-  }
-  // 每个点赞人以逗号加一个空格来相隔
-  var likesHtmlText = likesHtmlArr.join(', ');
-  htmlText.push(likesHtmlText);
-  htmlText.push('</div>');
-  return htmlText.join('');
->>>>>>> b15c21a11036c9e4ed7e12bc6ebe3b9c31c48318
 }
 /**
  * 评论内容 HTML 模板
@@ -220,7 +120,6 @@ function likesHtmlTpl(likes) {
  * @return {String} 返回html字符串
  */
 function commentsHtmlTpl(comments) {
-<<<<<<< HEAD
 	/*if (comments.length < 2) {
 		return '';
 	}*/
@@ -234,18 +133,6 @@ function commentsHtmlTpl(comments) {
 	}
 	htmlText.push('</div>');
 	return htmlText.join('');
-=======
-  if (!comments.length) {
-    return '';
-  }
-  var  htmlText = ['<div class="reply-comment">'];
-  for(var i = 0, len = comments.length; i < len; i++) {
-    var comment = comments[i];
-    htmlText.push('<div class="comment-item"><a class="reply-who" href="#">' + comment.author + '</a>：' + comment.text + '</div>');
-  }
-  htmlText.push('</div>');
-  return htmlText.join('');
->>>>>>> b15c21a11036c9e4ed7e12bc6ebe3b9c31c48318
 }
 /**
  * 评论点赞总体内容 HTML 模板
@@ -253,21 +140,12 @@ function commentsHtmlTpl(comments) {
  * @return {String} 返回html字符串
  */
 function replyTpl(replyData) {
-<<<<<<< HEAD
 	var htmlText = [];
 	htmlText.push('<div class="reply-zone">');
 	htmlText.push(likesHtmlTpl(replyData.likes, replyData.comments));
 	htmlText.push(commentsHtmlTpl(replyData.comments));
 	htmlText.push('</div>');
 	return htmlText.join('');
-=======
-  var htmlText = [];
-  htmlText.push('<div class="reply-zone">');
-  htmlText.push(likesHtmlTpl(replyData.likes));
-  htmlText.push(commentsHtmlTpl(replyData.comments));
-  htmlText.push('</div>');
-  return htmlText.join('');
->>>>>>> b15c21a11036c9e4ed7e12bc6ebe3b9c31c48318
 }
 /**
  * 多张图片消息模版 （可参考message.html）
@@ -275,7 +153,6 @@ function replyTpl(replyData) {
  * @return {String} 返回html字符串
  */
 function multiplePicTpl(pics) {
-<<<<<<< HEAD
 	var htmlText = [];
 	htmlText.push('<ul class="item-pic">');
 	for (var i = 0, len = pics.length; i < len; i++) {
@@ -390,70 +267,10 @@ var commentsButtom = [];
 var newReply = [];
 var getReply = [];
 var sent = [];
-=======
-  var htmlText = [];
-  htmlText.push('<ul class="item-pic">');
-  for (var i = 0, len = pics.length; i < len; i++) {
-    htmlText.push('<img class="pic-item" src="' + pics[i] + '">')
-  }
-  htmlText.push('</ul>');
-  return htmlText.join('');
-}
-
-/**
- * 循环：消息体 
- * @param {Object} messageData 对象
- */ 
-function messageTpl(messageData) {
-  var user = messageData.user;
-  var content = messageData.content;
-  var htmlText = [];
-  htmlText.push('<div class="moments-item" data-index="0">');
-  // 消息用户头像
-  htmlText.push('<a class="item-left" href="#">');
-  htmlText.push('<img src="' + user.avatar + '" width="42" height="42" alt=""/>');
-  htmlText.push('</a>');
-  // 消息右边内容
-  htmlText.push('<div class="item-right">');
-  // 消息内容-用户名称
-  htmlText.push('<a href="#" class="item-name">' + user.name + '</a>');
-  // 消息内容-文本信息
-  htmlText.push('<p class="item-msg">' + content.text + '</p>');
-  // 消息内容-图片列表 
-  var contentHtml = '';
-  // 目前只支持多图片消息，需要补充完成其余三种消息展示
-  switch(content.type) {
-      // 多图片消息
-    case 0:
-      contentHtml = multiplePicTpl(content.pics);
-      break;
-    case 1:
-      // TODO: 实现分享消息
-    case 2:
-      // TODO: 实现单张图片消息
-    case 3:
-      // TODO: 实现无图片消息
-  }
-  htmlText.push(contentHtml);
-  // 消息时间和回复按钮
-  htmlText.push('<div class="item-ft">');
-  htmlText.push('<span class="item-time">' + content.timeString + '</span>');
-  htmlText.push('<div class="item-reply-btn">');
-  htmlText.push('<span class="item-reply"></span>');
-  htmlText.push('</div></div>');
-  // 消息回复模块（点赞和评论）
-  htmlText.push(replyTpl(messageData.reply));
-  htmlText.push('</div></div>');
-  return htmlText.join('');
-}
-
-
->>>>>>> b15c21a11036c9e4ed7e12bc6ebe3b9c31c48318
 /**
  * 页面渲染函数：render
  */
 function render() {
-<<<<<<< HEAD
 	// TODO: 目前只渲染了一个消息（多图片信息）,需要展示data数组中的所有消息数据。
 	for (var i = 0; i < len; i++) {
 		var messageHtml = messageTpl(data[i]);
@@ -496,19 +313,10 @@ function render() {
 	}
 }
 
-=======
-  // TODO: 目前只渲染了一个消息（多图片信息）,需要展示data数组中的所有消息数据。
-  var messageHtml = messageTpl(data[0]);
-  $momentsList.innerHTML += messageHtml;
-}
-
-
->>>>>>> b15c21a11036c9e4ed7e12bc6ebe3b9c31c48318
 /**
  * 页面绑定事件函数：bindEvent
  */
 function bindEvent() {
-<<<<<<< HEAD
 	window.onload = function() {
 		document.getElementById("sent0").style.background = "#EFEFF4";
 
@@ -734,29 +542,15 @@ function bindEvent2() {
 }
 
 
-=======
-  // TODO: 完成页面交互功能事件绑定
-}
-
->>>>>>> b15c21a11036c9e4ed7e12bc6ebe3b9c31c48318
 /**
  * 页面入口函数：init
  * 1、根据数据页面内容
  * 2、绑定事件
  */
 function init() {
-<<<<<<< HEAD
 	// 渲染页面
 	render();
 	bindEvent2();
 }
 
 init();
-=======
-  // 渲染页面
-  render();
-  bindEvent();
-}
-
-init();
->>>>>>> b15c21a11036c9e4ed7e12bc6ebe3b9c31c48318
