b0deadcae918a34846c52a58b7f01ae5103ecb9f
LWD666 <39625238+LWD666@users.noreply.github.com>
2019-02-20 18:17:03 +0800

    2.20 updata

--- js/chat.js
+++ js/chat.js
@@ -1,50 +1,75 @@
 var data = {
-    "perception": {
-        "inputText": {
-            "text": ""
-        },
-    },
-    "userInfo": {
-        "apiKey": "a700c41c65de4895a164d88c98841422",
-        "userId": "402413"
-    }
+	"perception": {
+		"inputText": {
+			"text": ""
+		},
+	},
+	"userInfo": {
+		"apiKey": "a700c41c65de4895a164d88c98841422",
+		"userId": "402413"
+	}
 }
 
+// 监听plusready事件  
+document.addEventListener("plusready", function() {
+	// 扩展API加载完毕，现在可以正常调用扩展API
+	// ...
+}, false);
+var text = null;
+
+function startRecognize() {
+	var options = {};
+	options.engine = 'iFly';
+	text = "";
+	plus.speech.startRecognize(options, function(s) {
+		text += s;
+		document.getElementById('user-input').value = text;
+	}, function(e) {
+		alert("语音识别失败：" + e.message);
+	});
+	setTimeout(stopRecognize, 99999);
+}
+
+function stopRecognize() {
+	plus.speech.stopRecognize();
+}
+//渲染函数
+function render(turn, input) {
+	var htmlText =
+		`<img class="item-chat-${turn}" src="../img/FJ.jpg" />
+		<div id="test1" class="chat-text-${turn}">
+			<h4>${input}</h4>
+		</div>`;
+	var newLi = document.createElement("li");
+	newLi.classList.add("user-li-" + turn);
+	newLi.innerHTML = htmlText;
+	document.getElementById("chat-ul").appendChild(newLi);
+}
+
+var LR = ['left', 'right'];
 window.onload = function() {
+	//用户按下按键时判断
+	document.getElementById("user-input").onkeyup = function(){
+		if(document.getElementById('user-input').value != ''){
+			document.getElementById("input-sent").classList.remove("display-none");
+		}else{
+			document.getElementById("input-sent").classList.add("display-none");
+		}
+	}
 	//用户点击发送按钮
 	document.getElementById("input-sent").addEventListener("click", function() {
 		//发送input数据到text
 		var userInput = document.getElementById('user-input').value;
-		if(userInput == ''){
+		if (userInput == '') {
 			return false;
 		}
 		var test = data.perception.inputText.text;
 		data.perception.inputText.text = userInput;
 		//将用户输入渲染出来
-		var userLi = document.createElement("li");//用户回复总栏
-		userLi.classList.add("user-li-right");
-		var userImg = document.createElement("img");
-		userImg.classList.add("item-chat-right");
-		userImg.src = "../img/FJ.jpg";//用户头像
-		var userItem = document.createElement("div");//用户回复框
-		userItem.classList.add("chat-text-right");
-		var userItemArrow = document.createElement("div");//回复框尖角
-		userItemArrow.classList.add("arrow-right");
-		var userInputText = document.createElement("h4");//用户输入
-		var getInput = document.createTextNode(userInput);
-		userInputText.appendChild(getInput);
-		//拼接创建好的DOM
-		document.getElementById("chat-ul").appendChild(userLi);
-		userLi.appendChild(userImg);
-		userLi.appendChild(userItem);
-		userItem.appendChild(userItemArrow);
-		userItem.appendChild(userInputText);
-		//拼接完成后检测回复框高度避免遮挡
-		var userItemHeight = userItem.offsetHeight;//获得回复框的高度
-		userLi.style.height = userItemHeight + "px";
+		render(LR[1], userInput);
 		//发送后回到页面底部
 		var talkArea = document.getElementById('body');
-        talkArea.scrollTop = talkArea.scrollHeight;
+		talkArea.scrollTop = talkArea.scrollHeight;
 		//2.AJAX五部曲
 		var xhr = new XMLHttpRequest();
 		//创建链接
@@ -55,36 +80,19 @@ window.onload = function() {
 		xhr.send(newdata);
 		//监听状态变化
 		var a;
-		xhr.onreadystatechange = function(){
-			if(xhr.readyState == 4 && xhr.status == 200)
-			{
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4 && xhr.status == 200) {
 				var cb = xhr.responseText;
-				cb = eval('('+cb+')');
+				cb = eval('(' + cb + ')');
 				a = cb.results[0].values.text;
 				//将机器人的回复渲染出来
-				var cbLi = document.createElement("li");//机器人回复总栏
-				cbLi.classList.add("user-li-left");
-				var cbImg = document.createElement("img");
-				cbImg.classList.add("item-chat-left");
-				cbImg.src = "../img/FJ.jpg";//机器人头像
-				var cbItem = document.createElement("div");//机器人回复框
-				cbItem.classList.add("chat-text-left");
-				var cbItemArrow = document.createElement("div");//回复框尖角
-				cbItemArrow.classList.add("arrow-left");
-				var cbInputText = document.createElement("h4");
-				var getInput = document.createTextNode(a);
-				cbInputText.appendChild(getInput);
-				//拼接创建好的DOM
-				document.getElementById("chat-ul").appendChild(cbLi);
-				cbLi.appendChild(cbImg);
-				cbLi.appendChild(cbItem);
-				cbItem.appendChild(cbItemArrow);
-				cbItem.appendChild(cbInputText);
-				//拼接完成后检测回复框高度避免遮挡
-				var cbItemHeight = cbItem.offsetHeight;//获得回复框的高度
-				cbLi.style.height = cbItemHeight + "px";
+				render(LR[0], a);
+				//发送后回到页面底部
+				var talkArea = document.getElementById('body');
+				talkArea.scrollTop = talkArea.scrollHeight;
 			}
 		}
 		document.getElementById('user-input').value = '';
+		document.getElementById('input-sent').classList.add('display-none');
 	})
 }
